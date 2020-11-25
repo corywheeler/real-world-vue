5f397b62fe0aff5f98fac1f907b28e988c90a1a2
Cory Wheeler <corywheeler@gmail.com>
2020-11-25 10:48:29 -0500

    Init

---
+++ src/components/EventCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      something: 'This is a string'
+    }
+  },
+  name: 'EventCard.vue'
+}
+</script>
+
+<style scoped></style>
