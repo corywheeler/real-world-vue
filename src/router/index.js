e622372e3e62a54c8102ab2d3da849842c5dd411
Cory Wheeler <corywheeler@gmail.com>
2020-11-25 10:54:01 -0500

    Use an alias on a Route instead of Redirect, bad for SEO though

--- src/router/index.js
+++ src/router/index.js
@@ -12,16 +12,13 @@ const routes = [
   },
   {
     path: '/about-us',
+    alias: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/about',
-    redirect: { name: 'About' }
   }
 ]
 
