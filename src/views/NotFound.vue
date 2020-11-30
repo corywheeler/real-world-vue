8afc9da944859f9723441649f18c6a66d7d14ed1
Cory Wheeler <corywheeler@gmail.com>
2020-11-30 00:20:12 -0500

    404 NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Oops!</h1>
-    <h3>The page you're looking for is not here.</h3>
+    <h3>The {{ resource }} you're looking for is not here.</h3>
     <router-link :to="{ name: 'event-list' }"
       >Back to the home page</router-link
     >
@@ -10,7 +10,13 @@
 
 <script>
 export default {
-  name: 'NotFound.vue'
+  name: 'NotFound.vue',
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
 
