59656c6359b0686c58edfa013f3131b6f1c4c409
Cory Wheeler <corywheeler@gmail.com>
2020-12-07 12:00:20 -0500

    A new BaseSelect component

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,10 +2,12 @@
   <div>
     <!--    <h1>Create an event {{ user.name }}</h1>-->
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect
+        label="Select a category"
+        :options="categories"
+        v-model="event.category"
+      />
+
       <h3>Name & describe your event</h3>
       <BaseInput
         class="field"
